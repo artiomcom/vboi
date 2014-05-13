ac71515cabd8c6820d55ba1eab24d059cc5ec9e4
artiomcom <artiomcom@gmail.com>
2014-05-13 18:24:53 +0400

    adding hitComment()

--- FightWithRobotRoundLayout.php
+++ FightWithRobotRoundLayout.php
@@ -12,46 +12,45 @@ require_once('Judge.php');
 
 class FightWithRobotRoundLayout {
 
-    protected $dredd;
-
-    public function getRound($player1, $player2)
+     public function getRound($player1, $player2)
     {
         for ($i=0; $i < 10; $i++) {
+
             var_dump("Куда стрелять?");
 //           $line = readline("");
             $line = $this->read_stdin();
             $player1 = new gamer();
             $player1->setShootlocation($line);
-            $player1shootloc = $player1->getShootlocation();
+
             var_dump("Куда уклоняться?");
 //            $line1 = readline("");
             $line1 = $this->read_stdin();
             $player1->setLocation($line1);
-            $player1location = $player1->getLocation();
 
             $player2 = new robot();
             $player2->setShootlocation($player2->getRandomLocation());
-            $player2shootloc = $player2->getShootlocation();
-
             $player2->setLocation($player2->getRandomLocation());
-            $player2location = $player2->getLocation();
 
-            $this->dredd = new Judge();
-            $this->dredd->initFighters();
+            $dredd = new Judge();
+            $dredd->initFighters();
+            $dredd->shootComment($player1->getShootlocation(),   $player2->getShootlocation());
+            $dredd->ChangeLocComment($player1->getLocation(), $player2->getLocation());
+            $con = $dredd->shootComment($player1->getShootlocation(),   $player2->getShootlocation())
+                .". ".  $dredd->ChangeLocComment($player1->getLocation(), $player2->getLocation());
+            var_dump( $con );
+            var_dump($dredd->hitComment($player1,$player2));
 
-            $this->dredd->shootComment($player1shootloc,  $player2shootloc);
-            $this->dredd->ChangeLocComment($player1location, $player2location);
-//            $this->dredd->JudgeDreddComment($player1,$player2);
+
         }
     }
 
-   public  function read_stdin()
+   public  function read_stdin()//TRICK FOR WINDOWS
     {
         $fr=fopen("php://stdin","r");   // open our file pointer to read from stdin
         $input = fgets($fr,128);        // read a maximum of 128 characters
         $input = rtrim($input);         // trim any trailing spaces.
         fclose ($fr);                   // close the file handle
-        return $input;                  // return the text entered
+        return (int)$input;                  // return the text entered
     }
 
 } 
--- Judge.php
+++ Judge.php
@@ -12,8 +12,8 @@ require_once('gamer.php');
 class Judge
 {
 
-public $fighter1;
-public $fighter2;
+    public $fighter1;
+    public $fighter2;
 
     public function initFighters()
     {
@@ -24,37 +24,59 @@ public $fighter2;
     public function shootComment($fighter1ShootLoc, $fighter2ShootLoc)
     {
         $this->fighter1->setShootlocation($fighter1ShootLoc);
-        $fighter1shootresult = ($this->fighter1->getName() ." Стрелял в ". $this->fighter1->getShootlocation());
-
-        $this->fighter2 = new robot();
+        $fighter1shootresult = ($this->fighter1->getName() ." Стрелял в ". $this->fighter1->getShootlocation().", а");
         $this->fighter2->setShootlocation($fighter2ShootLoc);
         $fighter2shootresult = ($this->fighter2->getName() ." Стрелял в ". $this->fighter2->getShootlocation());
-
         $total = $fighter1shootresult ." ". $fighter2shootresult;
-        var_dump($total);
+//        var_dump($total);
+//        var_dump($this->hitComment( $this->fighter1,$this->fighter2 ));
         return $total;
     }
 
+    public function hitComment($fighter1,$fighter2)
+    {
+        $fighter1 = $this->fighter1;
+        $fighter2 = $this->fighter2;
 
+        $hitComm = "";
+
+        switch($hitComm)
+        {
+            case ($fighter1->getShootlocation()==$fighter2->getLocation()):
+                $hitComm = $fighter1->getName() . " HIT!";
+
+            case ($fighter1->getShootlocation()!=$fighter2->getLocation()):
+                $hitComm = $fighter1->getName() . " MISS!";
+
+            case ($fighter2->getShootlocation()==$fighter1->getLocation()):
+                $hitComm = $fighter2->getName() . " HIT!";
+            case ($fighter2->getShootlocation()!=$fighter1->getLocation()):
+                $hitComm = $fighter2->getName() . " MISS!";
+        }
+
+////        if ($this->fighter1->getShootlocation()==$this->fighter2->getLocation())
+////            var_dump("Bruce Lee HIT!");
+//            $hitComm = $this->fighter1->getName() . " HIT!";
+//        if ($this->fighter1->getShootlocation()!=$this->fighter2->getLocation())
+//            $hitComm = $this->fighter1->getName() . " MISS!";
+//
+//        if ($this->fighter2->getShootlocation()==$this->fighter1->getLocation())
+////            var_dump("ROBOCOB HIT");
+//            $hitComm = $this->fighter2->getName() . " HIT!";
+//        if ($this->fighter2->getShootlocation()!=$this->fighter1->getLocation())
+//            $hitComm = $this->fighter2->getName() . " MISS!";
+        return $hitComm;
+    }
 
     public function ChangeLocComment($fighter1tLoc, $fighter2Loc)
     {
         $this->fighter1->setLocation($fighter1tLoc);
-        $fighterlocresult = ($this->fighter1->getName() ." ушел в ". $this->fighter1->getLocation());
-
-        $this->fighter2 = new robot();
+        $fighterlocresult = ($this->fighter1->getName() ." ушел в ". $this->fighter1->getLocation().", а");
         $this->fighter2->setLocation($fighter2Loc);
         $fighter2shootresult = ($this->fighter2->getName() ." ушел в ". $this->fighter2->getLocation());
         $total = $fighterlocresult ." ". $fighter2shootresult;
-        var_dump($total);
+//        var_dump($total);
         return $total;
     }
 
-    public function JudgeDreddComment($player1, $player2)
-    {
-        $player1 = $this->fighter1;
-        $player2 = $this->fighter2;
-        $conc = (string)($this->shootComment($player1,$player2) . (string)$this->ChangeLocComment($player1,$player2));
-        var_dump((string)$this->shootComment($player1,$player2));
-    }
 }
